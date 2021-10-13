afd113c1deef9ef97b5b00fa0c62dcc16a1e9646
Chiamin Wu <chiaw@microsoft.com>
2021-10-13 11:39:21 -0700

    demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Dropdown, Toggle } from "@fluentui/react";
 
 function App() {
+  let displayNames = [];
+  let options = [];
+  const url =
+    "https://management.azure.com/subscriptions?api-version=2020-01-01";
+  const dropdownStyles = {
+    dropdown: { width: 300 },
+  };
+  function _onChange(ev, checked) {
+    console.log("toggle is " + (checked ? "checked" : "not checked"));
+  }
+  //update token everytime
+  const token = "123";
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url, {
+          method: "GET",
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        const json = await response.json();
+        json.value.map((o) => displayNames.push(o.id));
+        displayNames.forEach((name) => options.push({ key: name, text: name }));
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Dropdown
+        placeholder="Select a logical group"
+        label="Select a logical group"
+        options={options}
+        styles={dropdownStyles}
+      />
+      <Toggle
+        label="My Toggle example"
+        defaultChecked
+        onText="Active"
+        offText="Disabled"
+        onChange={_onChange}
+        role="checkbox"
+      />
+    </>
   );
 }
 
